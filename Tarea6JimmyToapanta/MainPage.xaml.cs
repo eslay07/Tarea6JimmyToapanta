7fe0f236a6a083cfbfcdd01388be8fb0b97ddaa6
jimmy omar <jimmy@gmail.com>
2022-11-12 23:33:16 -0500

    Agregar archivos de proyecto.

---
+++ Tarea6JimmyToapanta/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea6JimmyToapanta
+{
+    public partial class MainPage : ContentPage
+
+    {
+        private const string Url = "http://192.168.1.116/empleados/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Tarea6JimmyToapanta.ws.Datos> _post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<Tarea6JimmyToapanta.ws.Datos> posts=JsonConvert.DeserializeObject<List<Tarea6JimmyToapanta.ws.Datos>>(content);
+            _post = new ObservableCollection<Tarea6JimmyToapanta.ws.Datos>(posts);
+
+            MyListView.ItemsSource = _post;
+        }
+    }
+}
---
+++ Tarea6JimmyToapanta/ws/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea6JimmyToapanta.ws
+{
+    internal class Datos
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad { get; set; }
+        public string cedula { get; set; }
+        public string num_licencia { get; set; }
+        public string tipo_sangre { get; set; }
+    }
+}
